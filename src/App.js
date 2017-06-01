2426a8d37459d25d14159369655d2b2cf143f336
LAPTOP-NMSQ07ES\Mounika chowdary <dandamudi.mounica@gmail.com>
2017-06-01 14:51:42 -0500

    added data to list without mutation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
+import logo from './logo.svg';
 import './App.css';
-import {TodoForm, TodoList} from './Components/Todo'
+import {TodoForm, TodoList} from './components/todo'
 
 class App extends Component {
   constructor() {
@@ -26,6 +27,7 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
           <h2>React Todos</h2>
         </div>
         <div className="Todo-App">
